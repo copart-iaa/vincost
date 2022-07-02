85e019087f363cee7367f116d82a2f8b58328944
copart-iaa <107505308+copart-iaa@users.noreply.github.com>
2022-07-03 01:06:10 +0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'VIN Lookup history';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Copart and IAAI';
+    : 'Copart and IAAI check vin';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
