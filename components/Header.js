d453abf31d0a0bd062684d953f7f1ad00578347d
copart-iaa <107505308+copart-iaa@users.noreply.github.com>
2022-07-03 01:07:47 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
+        <Link href="https://vincost.com">
           <a>{name}</a>
         </Link>
       </p>
